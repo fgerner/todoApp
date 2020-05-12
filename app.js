7a2155c3205111a17e565c945482994ef104d6da
Fred <fred.gerner@icloud.com>
2020-05-12 22:33:39 +1000

    passing data from site to server

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const port = 3000;
+let items = [];
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
 
@@ -17,9 +19,15 @@ app.get('/', function (req, res) {
     };
 
     let day = today.toLocaleDateString("en-AU", options);
-    res.render('list', {dayOfWeek: day})
+    res.render('list', {dayOfWeek: day, newListItems: items})
 
 });
+
+app.post('/', function (req, res) {
+    items.push(req.body.newItem);
+    res.redirect('/')
+
+})
 
 app.listen(port, function () {
     console.log(`Server listening on port ${port}`);
