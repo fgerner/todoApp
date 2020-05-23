ef661ac96e584387f96734782b974f8810451714
Fred <fred.gerner@icloud.com>
2020-05-23 11:21:57 +1000

    remove item from database

--- app.js
+++ app.js
@@ -60,6 +60,17 @@ app.post('/', function (req, res) {
     res.redirect('/')
 });
 
+app.post('/delete', function (req, res) {
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/")
+        }
+    });
+});
+
 app.get('/work', function (req, res) {
     res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
