26ee166666fa1d4b5d42dd4b3e7bada062a37daf
Fred <fred.gerner@icloud.com>
2020-05-23 21:51:47 +1000

    add items to custom lists

--- app.js
+++ app.js
@@ -56,11 +56,22 @@ app.get('/:customListName', function (req, res) {
 
 app.post('/', function (req, res) {
     const itemName = req.body.newItem;
+    const listName = req.body.list;
     const newItem = new Item({
         name: itemName
     });
-    newItem.save()
-    res.redirect('/')
+
+    if (listName === 'Today') {
+        newItem.save()
+        res.redirect('/')
+    } else {
+        List.findOne({name: listName}, function (err, foundList) {
+                foundList.items.push(newItem);
+                foundList.save();
+                res.redirect('/' + listName);
+            }
+        )
+    }
 });
 
 app.post('/delete', function (req, res) {
